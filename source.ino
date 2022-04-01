31ac5752bfcd618f8c95853aa0ce4b23f7bb1679
GalteXx <65533265+GalteXx@users.noreply.github.com>
2022-04-01 15:37:08 +0300

    really easy one

---
+++ source.ino
@@ -0,0 +1,23 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(3, OUTPUT);
+  
+}
+
+void loop() {
+  int a = Serial.read();
+  a-= 48;
+  if(a == 1){
+    if(bitRead(PORTD, 3) == HIGH){
+      digitalWrite(3, LOW);
+      Serial.write("off");
+    }
+      
+    else{
+      digitalWrite(3, HIGH);
+      Serial.write("on");
+    }
+  }
+    
+    
+}
